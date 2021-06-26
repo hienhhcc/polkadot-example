a7f4a521912c0f46a86d22a8d5c8da72097b07ec
hienhhcc <thachdau16t@gmail.com>
2021-06-26 20:10:17 +0700

    handle pallet storage

--- pallets/template/src/lib2.rs
+++ pallets/template/src/lib2.rs
@@ -43,7 +43,8 @@ pub mod pallet {
     #[pallet::generate_store(pub(super) trait Store)]
     pub struct Pallet<T>(_);
     
-    #[pallet::storage] // <-- Step 5. code block will replace this.
+    #[pallet::storage] 
+    pub(super) type Proofs<T: Config> = StorageMap<_, Blake2_128Concat, Vec<u8>, (T::AccountId, T::BlockNumber), ValueQuery>;   
     
     #[pallet::hooks]
     impl<T: Config> Hooks<BlockNumberFor<T>> for Pallet<T> {}
